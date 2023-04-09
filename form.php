cda595b7420f1955647c666bde0703973c27206b
nana579 <125179015+nana579@users.noreply.github.com>
2023-04-09 11:52:31 +0900

    Update form.php

--- form.php
+++ form.php
@@ -1,33 +1,27 @@
 <?php
-session_start();   //SESSIONを使うときは最初にスタートさせる
-// 送信ボタンが押されたかどうか
+session_start();
 if (isset($_POST['submit'])) {
-  // POSTされたデータをエスケープ処理して変数に格納
   $name  = htmlspecialchars($_POST['name'], ENT_QUOTES | ENT_HTML5);
   $email = htmlspecialchars($_POST['email'], ENT_QUOTES | ENT_HTML5);
   $contents = htmlspecialchars($_POST['contents'], ENT_QUOTES | ENT_HTML5);
-  $errors = []; //エラー格納用配列
+  $errors = []; 
   if (trim($name) === '' || trim($name) === "　") {
     $errors['name'] = "名前を入力してください";
   }
   if (trim($contents) === '' || trim($contents) === "　") {
     $errors['contents'] = "内容を入力してください";
   }
-  // エラー配列ががなければ完了
   if (count($errors) === 0) {
-    //⇦エスケープ処理をして値を変数に格納済みの入力値
     $_SESSION['name'] = $name;
     $_SESSION['email'] = $email;
     $_SESSION['contents'] = $contents;
 //     header('Location:http://localhost/hatchwebsite/confirm.php');
     header('Location:https://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/confirm.php');
   } else {
-    // エラー配列があればエラーを表示
     echo $errors['name'];
     echo $errors['contents'];
   }
 }
-// confirm.phpから戻ってきたときに値を保持
 if (isset($_GET) && isset($_GET['action']) && $_GET['action'] === 'edit') {
   $name  = $_SESSION['name'];
   $email = $_SESSION['email'];
@@ -48,7 +42,6 @@ if (isset($_GET) && isset($_GET['action']) && $_GET['action'] === 'edit') {
 <body>
   <form action="form.php" method="post">
     <p>お名前</p>
-    <!-- //input部分で変更したのはvalueの部分です -->
     <input type="text" name="name" value="<?php if (isset($name)) {
                                             echo $name;
                                           } ?>" placeholder="お名前" required>
@@ -57,7 +50,6 @@ if (isset($_GET) && isset($_GET['action']) && $_GET['action'] === 'edit') {
                                               echo $email;
                                             } ?>" placeholder="メールアドレス" required>
     <p>内容</p>
-    <!-- //textareaのみvalueではなく要素の間なので注意してください⇩ -->
     <textarea type="text" name="contents" placeholder="お問い合わせ内容" rows="7" required><?php if (isset($contents)) {
                                                                                       echo $contents;
                                                                                     } ?></textarea>
